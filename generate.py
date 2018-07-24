efb5259c85517a8ab38ed107deb60d69accf1c68
Yuki Koyama <yuki@koyama.xyz>
2018-07-24 20:31:00 +0900

    Add support of generating div blocks

--- generate.py
+++ generate.py
@@ -1,7 +1,14 @@
 import csv
 
-csv_file = open("./data.csv", "r")
-member_list = csv.reader(csv_file, delimiter=",")
+csv_file = open('./data.csv', 'r')
+member_list = csv.reader(csv_file, delimiter=',')
+
+html = ''
 
 for row in member_list:
-    print(row)
+	item_html = '<div class="item"></div>'
+	html += item_html + '\n'
+
+csv_file.close()
+
+print(html)
